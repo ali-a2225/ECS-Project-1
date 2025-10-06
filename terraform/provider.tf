b3d1523ec2fbf7e9bd48a81faf3118c3b50a7217
ali-a2225 <alikabukar@gmail.com>
2025-10-06 18:11:27 +0100

    add state locking with s3 bucket

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,11 @@
 terraform {
   required_providers {
     aws = { source = "hashicorp/aws", version = "6.11.0" }
+  }
+    backend "s3" {
+      bucket = "terraform-state-aws-ali"
+      key    = "terraform.tfstate"
+      region = "eu-west-2"
     }
 }
 provider "aws" { region = "eu-west-2"}
