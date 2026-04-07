55a9f4a2631a2c1fbacbd4c9c901d02b42a68bbb
ali-a2225 <alikabukar@gmail.com>
2026-04-07 22:13:11 +0100

    add tf_state_bucket_name

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,4 @@ variable "hostPort" {type = number}
 variable "app_name" {type = string}
 variable "web_ecs_cluster" {type = string}
 variable "containerPort" {type = number}
+variable "tf_state_bucket_name" {type = string}
