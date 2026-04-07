295b5448ca52d35e9c15709de41ac311402a792f
ali-a2225 <alikabukar@gmail.com>
2026-04-08 00:23:30 +0100

    add certificate validation

--- terraform/acm/acm.tf
+++ terraform/acm/acm.tf
@@ -1,8 +1,30 @@
-data "aws_acm_certificate" "cert" {
-  domain   = "aliabukar.com"
-  statuses = ["ISSUED"]
+# data "aws_acm_certificate" "cert" {
+#   domain   = var.domain_name
+#   statuses = ["ISSUED"]
+# }
+
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
-output "cert_arn" {
-  value = data.aws_acm_certificate.cert.arn
+resource "aws_acm_certificate_validation" "cert_validation" {
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.dm : record.fqdn]
 }
+
+resource "aws_acm_certificate_validation" "main" {
+  certificate_arn = aws_acm_certificate.cert.arn
+
+  validation_record_fqdns = [
+    for record in aws_route53_record.cert_validation : record.fqdn
+  ]
+}
+
+# output "cert_arn" {
+#   value = data.aws_acm_certificate.cert.arn
+# }
