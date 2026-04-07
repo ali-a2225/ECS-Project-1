370a5dfecc1479e451f37b91b5ee53c572135a98
ali-a2225 <alikabukar@gmail.com>
2026-04-08 00:02:01 +0100

    add domain name value

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,3 +3,4 @@ variable "app_name" {type = string}
 variable "web_ecs_cluster" {type = string}
 variable "containerPort" {type = number}
 variable "tf_state_bucket_name" {type = string}
+variable "domain_name" {type = string}
