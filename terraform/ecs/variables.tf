b84ebbbb4b7ddf0beec9245ab6caf4334a140a68
ali-a2225 <alikabukar@gmail.com>
2026-04-07 07:50:51 +0100

    add variables

--- terraform/ecs/variables.tf
+++ terraform/ecs/variables.tf
@@ -1,3 +1,4 @@
+variable "vpc_id" {}
 variable "web_asg_arn" {}
 variable "ECS_Agent_Role_ARN" {}
 variable "private_subnets" {
@@ -9,5 +10,9 @@ variable "containerPort" {
   type = number
 }
 variable "hostPort" {
+  default = 8080
   type = number
 }
+variable "app_name" {type = string}
+variable "web_ecs_cluster" { type = string}
+variable "app_name" {type = string}
