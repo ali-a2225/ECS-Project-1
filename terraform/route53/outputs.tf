17f324660d2aa7ab71835afa84c5a9ce37f83499
ali-a2225 <alikabukar@gmail.com>
2026-04-08 18:03:05 +0100

    fix outputs and variables

--- terraform/route53/outputs.tf
+++ terraform/route53/outputs.tf
@@ -1,3 +1,7 @@
 output "route53_record_name" {
-    value = aws_route53_record.dm.name
+    value = [aws_route53_record.dm.name]
+}
+output "cert_validation" {
+  value       = [for record in aws_route53_record.cert_validation : record.fqdn]
+  description = "List of domain validation record names for ACM certificate"
 }
--- terraform/route53/variables.tf
+++ terraform/route53/variables.tf
@@ -1,5 +1,12 @@
 variable "alb_zone_id" {}
 variable "alb_url" {}
-variable "domain_name" {
-  default = "aliabukar.com"
+variable "domain_name" {}
+variable "cert_arn" {}
+variable "domain_validation_options" {
+    type = list(object({
+        domain_name = string
+        resource_record_name = string
+        resource_record_type = string
+        resource_record_value = string
+    }))
 }
