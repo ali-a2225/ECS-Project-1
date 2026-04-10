a8757b237ded4daa0f974001de1b56a4b8aaa26e
ali-a2225 <alikabukar@gmail.com>
2026-04-10 16:53:20 +0100

    add null provider

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,6 +1,7 @@
 terraform {
   required_providers {
     aws = { source = "hashicorp/aws", version = "6.11.0" }
+    null = {source  = "hashicorp/null", version = "3.2.4"}
   }
     backend "s3" {
       bucket = "terraform-state-aws-ali"
