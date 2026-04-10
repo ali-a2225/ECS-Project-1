4484adfda6c5878924c58841dc613990ebe09db1
ali-a2225 <alikabukar@gmail.com>
2026-04-10 18:42:47 +0100

    add godaddy vars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,3 +4,15 @@ variable "web_ecs_cluster" {type = string}
 variable "containerPort" {type = number}
 variable "tf_state_bucket_name" {type = string}
 variable "domain_name" {type = string}
+variable "GODADDY_API_KEY" {
+  description = "GoDaddy API Key"
+  type        = string
+  sensitive   = true
+  default = ""
+}
+variable "GODADDY_API_SECRET" {
+  description = "GoDaddy API Secret"
+  type        = string
+  sensitive   = true
+  default = ""
+}
