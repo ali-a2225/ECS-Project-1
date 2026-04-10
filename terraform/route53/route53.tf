c7105879781e65a82ce1a01d4ed76d6da76beca0
ali-a2225 <alikabukar@gmail.com>
2026-04-10 08:10:44 +0100

    auto create DNS records for ACM validation

--- terraform/route53/route53.tf
+++ terraform/route53/route53.tf
@@ -1,10 +1,33 @@
-resource "aws_route53_zone" "main" {
+data "aws_route53_zone" "main" {
   name         = var.domain_name
+}
+
+# Validation records
+## detects what ACM needs and automatically creates the records needed by ACM certificate
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in var.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+  
+  #create the record in Route53 to validate the ACM certificate
+  name    = each.value.name
+  records = [each.value.record]
+  type    = each.value.type
+  ttl     = 60
+  zone_id = data.aws_route53_zone.main.zone_id
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 #DNS name to Load Balancer Domain Name mapping
 resource "aws_route53_record" "dm" {
-  zone_id = aws_route53_zone.main.zone_id
+  zone_id = data.aws_route53_zone.main.zone_id
   name    = var.domain_name
   type    = "A"
 
@@ -17,7 +40,7 @@ resource "aws_route53_record" "dm" {
 
 # Subdomain -> ALB
 resource "aws_route53_record" "tm_dm" {
-  zone_id = aws_route53_zone.main.zone_id
+  zone_id = data.aws_route53_zone.main.zone_id
   name    = "tm.${var.domain_name}"
   type    = "A"
 
