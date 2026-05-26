5360b217a5d2182670c566679da9e849320cc79d
ali-a2225 <alikabukar@gmail.com>
2026-05-26 06:26:19 +0100

    rename resources to compute

--- terraform/modules/resources/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "web_asg_arn" {
-    value = aws_autoscaling_group.web_asg.arn
-    description = "Auto Scaling Group arn"
-}
--- terraform/modules/resources/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "vpc_id"  {}
-variable "web_sg_id" {}
-variable "EC2_Instance_Profile_ARN" {}
-variable "web_ecs_cluster_name" {}
-variable "private_subnets" {
-  type = list(string)
-}
-variable "target_group_arn" {}
