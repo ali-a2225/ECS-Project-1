ef59f419834f77c8262343d58dac4d149a6a3082
ali-a2225 <alikabukar@gmail.com>
2026-05-29 23:13:24 +0100

    delete unused variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,7 +2,6 @@ variable "hostPort" {type = number}
 variable "app_name" {type = string}
 variable "web_ecs_cluster" {type = string}
 variable "containerPort" {type = number}
-variable "tf_state_bucket_name" {type = string}
 variable "domain_name" {type = string}
 variable "GODADDY_API_KEY" {
   description = "GoDaddy API Key"
