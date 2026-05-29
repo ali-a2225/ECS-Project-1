7658674f7ad14ddf273c4b2d8ef23344f8e763a1
ali-a2225 <alikabukar@gmail.com>
2026-05-30 00:02:11 +0100

    add AZ variable + resolve extra

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,5 @@
+variable "az_count" {
+  description = "Number of AZs to spread subnets across"
+  type        = number
+  default     = 3
+}
--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -82,7 +82,7 @@ resource "aws_route_table_association" "public" {
 resource "aws_subnet" "private" {
   count             = 3
   vpc_id            = aws_vpc.main.id
-  cidr_block        = cidr_block = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 11)
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index + 11)
   availability_zone = data.aws_availability_zones.available.names[count.index]
 
   tags = {
